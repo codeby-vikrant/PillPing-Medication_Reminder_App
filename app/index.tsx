6f8a88eb679cbc70c233dfbd77b77ae7fc71d00d
codeby-vikrant <workwithvikrant0111@gmail.com>
2026-01-18 14:51:52 +0530

    created new file for splashscreen

---
+++ app/index.tsx
@@ -0,0 +1,7 @@
+import {View, Text, StyleSheet, Animated} from 'react-native';
+import {Ionicons} from '@expo/vector-icons';
+import { useEffect, useRef } from 'react';
+
+export default function SplashScreen(){
+    
+}
