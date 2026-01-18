c4c9a937c5c828ca1eb653963df7c6af75a4460a
codeby-vikrant <workwithvikrant0111@gmail.com>
2026-01-18 15:01:50 +0530

    updated existing index.tsx

--- app/index.tsx
+++
@@ -1,7 +0,0 @@
-import {View, Text, StyleSheet, Animated} from 'react-native';
-import {Ionicons} from '@expo/vector-icons';
-import { useEffect, useRef } from 'react';
-
-export default function SplashScreen(){
-    
-}
