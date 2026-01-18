bf63f5f3a3daaf36bc6da83f0d001ae384385f2a
codeby-vikrant <workwithvikrant0111@gmail.com>
2026-01-18 20:16:17 +0530

    changed bg color of splash screen

--- app/index.tsx
+++ app/index.tsx
@@ -49,7 +49,7 @@ export default function SplashScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#90e0ef",
+    backgroundColor: "#0077b6",
     alignItems: "center",
     justifyContent: "center",
   },
