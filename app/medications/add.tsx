930ff510647b6857022ebc8e5368344a5b4e1d8e
codeby-vikrant <workwithvikrant0111@gmail.com>
2026-01-20 13:41:58 +0530

    created a new file for adding medications

---
+++ app/medications/add.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+  Switch,
+  Dimensions,
+  Platform,
+  Alert,
+} from "react-native";
+
+export default function AddMedicationScreen() {
+  return <View></View>;
+}
