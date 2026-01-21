52a64be61d8b66701d749509ebf1653b75f17c20
codeby-vikrant <workwithvikrant0111@gmail.com>
2026-01-22 00:09:52 +0530

    created functionality to display calendar and show if dose taken on any day

--- app/calendar/index.tsx
+++ app/calendar/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, JSX } from "react";
 import {
   View,
   Text,
@@ -10,12 +10,84 @@ import {
 import { useRouter } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
 import { LinearGradient } from "expo-linear-gradient";
+import {
+  DoseHistory,
+  getDoseHistory,
+  getMedication,
+  Medication,
+} from "@/utils/storage";
+import { useFocusEffect } from "@react-navigation/native";
 
 const WEEKDAYS = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
 export default function CalendarScreen() {
   const router = useRouter();
   const [selectedDate, setSelectedDate] = useState(new Date());
+  const [medications, setMedications] = useState<Medication[]>([]);
+  const [doseHistory, setDoseHistory] = useState<DoseHistory[]>([]);
+
+  const loadData = useCallback(async () => {
+    try {
+      const [meds, history] = await Promise.all([
+        getMedication(),
+        getDoseHistory(),
+      ]);
+
+      setMedications(meds);
+      setDoseHistory(history);
+    } catch (error) {
+      console.error("Error Loading Calendar Data", error);
+    }
+  }, [selectedDate]);
+
+  useFocusEffect(
+    useCallback(() => {
+      loadData();
+    }, [loadData]),
+  );
+
+  const getDaysInMonth = (date: Date) => {
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    const days = new Date(year, month + 1, 0).getDate();
+    const firstDay = new Date(year, month, 1).getDay();
+    return { days, firstDay };
+  };
+
+  const { days, firstDay } = getDaysInMonth(selectedDate);
+
+  const renderCalendar = () => {
+    const calendar: JSX.Element[] = [];
+    let week: JSX.Element[] = [];
+
+    for (let i = 0; i < firstDay; i++) {
+      week.push(<View key={`empty-${i}`} />);
+    }
+
+    for (let day = 1; day <= days; day++) {
+      const date = new Date(
+        selectedDate.getFullYear(),
+        selectedDate.getMonth(),
+      );
+      const isToday = new Date().toDateString() === date.toDateString();
+      const hasDoses = doseHistory.some(
+        (dose) =>
+          new Date(dose.timestamp).toDateString() === date.toDateString(),
+      );
+      week.push(
+        <TouchableOpacity key={day}>
+          <Text>{day}</Text>
+          {hasDoses && <View />}
+        </TouchableOpacity>,
+      );
+
+      if ((firstDay + day) % 7 === 0 || day === days) {
+        calendar.push(<View key={day}>{week}</View>);
+        week = [];
+      }
+    }
+    return calendar;
+  };
 
   return (
     <View>
