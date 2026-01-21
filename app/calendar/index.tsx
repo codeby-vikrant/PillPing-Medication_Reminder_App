8af1bb57d717a200a8ac2e8c9675d688ca1beee0
codeby-vikrant <workwithvikrant0111@gmail.com>
2026-01-21 23:27:57 +0530

    created a new file for calendar screen

---
+++ app/calendar/index.tsx
@@ -0,0 +1,13 @@
+import React, { useState, useCallback } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+  Platform,
+} from "react-native";
+import { useRouter } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import { LinearGradient } from "expo-linear-gradient";
+
