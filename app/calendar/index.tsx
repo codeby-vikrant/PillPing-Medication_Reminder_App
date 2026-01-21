7f5f8533c955fba337395f56e55000a3e4af02cb
codeby-vikrant <workwithvikrant0111@gmail.com>
2026-01-21 23:49:29 +0530

    created a structure for calendar screen with back button and title

--- app/calendar/index.tsx
+++ app/calendar/index.tsx
@@ -11,3 +11,59 @@ import { useRouter } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
 import { LinearGradient } from "expo-linear-gradient";
 
+const WEEKDAYS = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+export default function CalendarScreen() {
+  const router = useRouter();
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  return (
+    <View>
+      <LinearGradient
+        colors={["#0077b6", "#90e0ef"]}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 0 }}
+      ></LinearGradient>
+      <View>
+        <View>
+          <TouchableOpacity>
+            <Ionicons name="chevron-back" size={28} color={"#0077b6"} />
+          </TouchableOpacity>
+        </View>
+        <Text>Calendar</Text>
+      </View>
+
+      <View>
+        <View>
+          <TouchableOpacity>
+            <Ionicons name="chevron-back" size={24} color={"#333"} />
+          </TouchableOpacity>
+          <Text>
+            {selectedDate.toLocaleString("default", {
+              month: "long",
+              year: "numeric",
+            })}
+          </Text>
+          <TouchableOpacity>
+            <Ionicons name="chevron-forward" size={24} color={"#333"} />
+          </TouchableOpacity>
+        </View>
+        <View>
+          {WEEKDAYS.map((day) => (
+            <Text key={day}>{day}</Text>
+          ))}
+        </View>
+        <View>
+          <Text>
+            {selectedDate.toLocaleDateString("default", {
+              weekday: "long",
+              month: "long",
+              day: "numeric",
+            })}
+          </Text>
+          <ScrollView></ScrollView>
+        </View>
+      </View>
+    </View>
+  );
+}
